8b891e8569d5b485502c0d59a60e581e1bb9a74d
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-21 10:09:17 -0700

    Make the Group Profile page visible to everyone

--- src/components/nexus/groups/group-detail/groupTabs.ts
+++ src/components/nexus/groups/group-detail/groupTabs.ts
@@ -51,12 +51,14 @@ export const availableTabs = (userRole: MembershipRole): Tab[] => {
       requiredRole: [MembershipRole.Admin],
     },
     { key: TabKey.GroupRequests, name: 'Requests', requiredRole: [MembershipRole.Admin] },
+    { key: TabKey.GroupSettings, name: 'Settings', requiredRole: [MembershipRole.Admin] },
+
+    // Visible to all members
     {
       key: TabKey.GroupProfile,
       name: 'Profile',
-      requiredRole: [MembershipRole.Admin, MembershipRole.Trader],
+      requiredRole: [MembershipRole.Admin, MembershipRole.Trader, MembershipRole.Member],
     },
-    { key: TabKey.GroupSettings, name: 'Settings', requiredRole: [MembershipRole.Admin] },
   ]
 
   return allTabs
