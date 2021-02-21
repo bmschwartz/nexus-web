fa933af2675b564f8ce29ce13defadd8491c5a65
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-21 10:18:49 -0700

    Basic text area field to edit group description

--- src/components/nexus/groups/group-detail/GroupProfile.tsx
+++ src/components/nexus/groups/group-detail/GroupProfile.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react'
 import { Group } from 'types/group'
+import { Divider, Input } from 'antd'
 
 interface GroupProfileProps {
   group: Group
 }
 
 export const GroupProfile: FC<GroupProfileProps> = ({ group }) => {
-  return <p>Profile for {group.name}</p>
+  return (
+    <>
+      <Divider orientation="left">
+        <strong>Edit Profile</strong>
+      </Divider>
+      <div style={{ width: 600 }}>
+        <Input.TextArea defaultValue={group.description} maxLength={500} />
+      </div>
+    </>
+  )
 }
