c54a244ef227a1f9c077d40fe5ec43b0bf379118
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-09 21:22:21 -0700

    Added GroupMembers table

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 
 /* eslint-disable */
 import * as apollo from '../../services/apollo'
+import { useGetMyGroupQuery } from '../../graphql'
+import { Spin } from 'antd'
+import { transformGroups } from '../../types/group'
+import {
+  GroupMembers,
+  GroupMembersTabState,
+} from '../../components/nexus/groups/group-detail/GroupMembers'
 /* eslint-enable */
 
 const Members = () => {
+  const { data, loading } = useGetMyGroupQuery({ fetchPolicy: 'cache-and-network' })
+  const [groupMembersTabState, setGroupMembersTabState] = useState<GroupMembersTabState>(
+    GroupMembersTabState.VIEW_ALL,
+  )
+
+  if (loading || !data?.myGroup) {
+    return <Spin />
+  }
+
+  const group = transformGroups([data.myGroup])[0]
+
   if (!apollo.isGroupOwnerOrTraderUserType()) {
     return <Redirect to="/" />
   }
 
   return (
     <div>
-      <h3>Members</h3>
+      <GroupMembers
+        group={group}
+        tabState={groupMembersTabState}
+        setTabState={setGroupMembersTabState}
+      />
     </div>
   )
 }
