9173657e065f5e54f49c7ffec64b74f327d94d1c
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-13 16:55:49 -0700

    Set max number on Duration and Fee

--- src/components/nexus/groups/create-group/createGroupFormUtils.tsx
+++ src/components/nexus/groups/create-group/createGroupFormUtils.tsx
@@ -9,14 +9,14 @@ export const membershipTableColumns = [
     dataIndex: 'duration',
     key: 'duration',
     width: '10%',
-    render: () => <InputNumber min={1} step={1} defaultValue={1} />,
+    render: () => <InputNumber min={1} step={1} max={12} defaultValue={1} />,
   },
   {
     title: 'Fee (USD)',
     dataIndex: 'fee',
     key: 'fee',
     width: '25%',
-    render: () => <Input addonBefore="$" type="number" min={0} step={1} defaultValue={100} />,
+    render: () => <InputNumber min={0} max={999999} step={1} defaultValue={100} />,
   },
   {
     title: 'Description (Optional)',
