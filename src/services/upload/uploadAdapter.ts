85523956bafb7d9bb884332793c785014f7ee05b
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-20 19:10:11 -0600

    Make sure the selected "image" file really is an image

--- src/services/upload/uploadAdapter.ts
+++ src/services/upload/uploadAdapter.ts
@@ -1,6 +1,7 @@
 import { v4 } from 'uuid'
 /* eslint-disable */
 import { firebase } from '../firebase'
+import imageType from 'image-type'
 /* eslint-enable */
 
 export class UploadAdapter {
@@ -12,38 +13,45 @@ export class UploadAdapter {
 
   upload() {
     return this.loader.file.then(
-      (file: any) =>
-        new Promise(resolve => {
-          // firebase reference
-          const extension = file.name.split('.').pop()
-          const fileName = `${v4()}.${extension}`
-          const uploadTask = firebase
-            .storage()
-            .ref()
-            .child(`/images/${fileName}`)
-            .put(file)
+      (file: File) =>
+        new Promise((resolve, reject) => {
+          file.arrayBuffer().then((arrayBuffer: ArrayBuffer) => {
+            const buffer = Buffer.from(arrayBuffer)
+            const type = imageType(buffer)
+            if (!type?.mime.startsWith('image/')) {
+              reject(new Error('Not a valid image!'))
+              return
+            }
+            const extension = file.name.split('.').pop()
+            const fileName = `${v4()}.${extension}`
+            const uploadTask = firebase
+              .storage()
+              .ref()
+              .child(`/images/${fileName}`)
+              .put(file)
 
-          uploadTask.on(
-            firebase.storage.TaskEvent.STATE_CHANGED,
-            snapshot => {
-              /* snapshot with info about 
-              the upload progress & metadata */
-              console.log(snapshot)
-            },
-            error => {
-              // error handling
-              console.error(error)
-            },
-            () => {
-              // upload successful
-              uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-                console.log(`upload success: ${downloadURL}`)
-                resolve({
-                  default: downloadURL,
+            uploadTask.on(
+              firebase.storage.TaskEvent.STATE_CHANGED,
+              snapshot => {
+                /* snapshot with info about
+                the upload progress & metadata */
+                console.log(snapshot)
+              },
+              error => {
+                // error handling
+                console.error(error)
+              },
+              () => {
+                // upload successful
+                uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+                  console.log(`upload success: ${downloadURL}`)
+                  resolve({
+                    default: downloadURL,
+                  })
                 })
-              })
-            },
-          )
+              },
+            )
+          })
         }),
     )
   }
