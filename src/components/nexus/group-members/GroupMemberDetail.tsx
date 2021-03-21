91e91d1e9ff318d695f2e3edb9fdbafc3c0fe43a
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-20 23:20:27 -0600

    Strip out the "Remove Member" functionality

--- src/components/nexus/group-members/GroupMemberDetail.tsx
+++ src/components/nexus/group-members/GroupMemberDetail.tsx
@@ -1,16 +1,14 @@
 import React, { FC, useState } from 'react'
-import { Button, Divider, Modal, notification, PageHeader, Spin } from 'antd'
-import * as apollo from 'services/apollo'
+import { Divider, PageHeader, Spin } from 'antd'
 
 /* eslint-disable */
-import { MembershipRole as RemoteMembershipRole, useGetGroupMemberQuery } from '../../../graphql'
+import { useGetGroupMemberQuery } from '../../../graphql'
 import { displayTimeBeforeNow } from '../dateUtil'
 import {
   convertToLocalMembershipRole,
   convertToLocalMembershipStatus,
   MembershipRole,
 } from 'types/membership'
-import { ExclamationCircleOutlined } from '@ant-design/icons'
 
 /* eslint-enable */
 
@@ -20,20 +18,7 @@ interface GroupMemberDetailProps {
   onRemovedMember: () => void
 }
 
-const canRemoveMember = (memberRole: RemoteMembershipRole) => {
-  const localRole = convertToLocalMembershipRole(memberRole)
-  if (!localRole) {
-    return false
-  }
-  return [MembershipRole.Member, MembershipRole.Trader].includes(localRole)
-}
-
-export const GroupMemberDetail: FC<GroupMemberDetailProps> = ({
-  groupMemberId,
-  onClickBack,
-  onRemovedMember,
-}) => {
-  const [removingMember, setRemovingMember] = useState<boolean>(false)
+export const GroupMemberDetail: FC<GroupMemberDetailProps> = ({ onClickBack, groupMemberId }) => {
   const [selectedMemberRole, setSelectedMemberRole] = useState<MembershipRole | null>()
 
   const { data: memberData, loading: fetchingMemberData } = useGetGroupMemberQuery({
@@ -43,41 +28,6 @@ export const GroupMemberDetail: FC<GroupMemberDetailProps> = ({
 
   if (!selectedMemberRole && membership && membership.role) {
     setSelectedMemberRole(convertToLocalMembershipRole(membership.role))
-  }
-
-  const onClickRemoveMember = async () => {
-    Modal.confirm({
-      title: `Removing ${membership?.member.username} from ${membership?.group.name}`,
-      icon: <ExclamationCircleOutlined />,
-      content: `Are you sure you want to remove ${membership?.member.username}?`,
-      okText: 'Yes',
-      okType: 'danger',
-      async onOk() {
-        setRemovingMember(true)
-
-        const groupId = membership!.group.id
-        const membershipId = membership!.id
-        const { error, success } = await apollo.removeMember({ groupId, membershipId })
-
-        setRemovingMember(false)
-
-        if (success) {
-          notification.success({
-            message: `Removed ${membership?.member.username}`,
-          })
-        } else {
-          notification.error({
-            message: `Error Removing ${membership?.member.username}`,
-            description: error,
-            duration: 3, // seconds
-          })
-          return
-        }
-
-        onRemovedMember()
-      },
-      onCancel() {},
-    })
   }
 
   return (
@@ -119,18 +69,6 @@ export const GroupMemberDetail: FC<GroupMemberDetailProps> = ({
             <strong className="mr-3">Total Orders</strong>
             {membership && membership.orders.totalCount}
           </div>
-
-          {membership && canRemoveMember(membership.role) && (
-            <div className="d-flex flex-nowrap align-items-center mt-3 pb-3 pl-4 pr-4">
-              <Button
-                danger
-                disabled={fetchingMemberData || removingMember}
-                onClick={() => onClickRemoveMember()}
-              >
-                Remove Member
-              </Button>
-            </div>
-          )}
         </div>
       </Spin>
     </>
